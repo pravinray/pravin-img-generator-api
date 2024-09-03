56478bedbfd352c734fcc563cacae1ef771adb8f
Pravin Ray <pravin@Pravins-MacBook-Pro.local>
2024-09-03 17:51:35 +0545

    added to generate image from limewire

---
+++ routes/generateOtp.js
@@ -0,0 +1,11 @@
+
+
+const generateOTP = () => {
+  const OTP = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
+  return OTP;
+};
+
+module.exports = generateOTP();
